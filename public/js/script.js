8b33f81da8d214c1f28b363adabe70438eb5c1b0
Nicolas Marteau <nico_mart@hotmail.fr>
2019-05-07 11:10:35 +0200

    chore: change addon's blocks color

--- public/js/script.js
+++ public/js/script.js
@@ -7,13 +7,23 @@
 
     // Animation addon list
     for (const div of blockList) {
+        // Init const
         const target = div.style;
+        const targetP = div.children[0].style;
+
+        // Event click
         div.addEventListener("click", function(e) {
             init();
+            // Style div
             target.backgroundColor = "rgba(255, 151, 3, .5)";
             target.boxShadow = "0 0 5px #404040";
             target.borderColor = "black";
 
+            // Style p
+            targetP.backgroundColor = "#111";
+            targetP.color = "white";
+
+            // Req ajax
             req.open("GET", "http://localhost:8000/infos", true);
             req.send(null);
             req.onload = function(data) {
@@ -22,9 +32,12 @@
             }
 
         });
+
+        // Events mouse
         div.addEventListener("mouseover", function() {
             target.boxShadow = "0 0 5px #404040";
         })
+
         div.addEventListener("mouseout", function() {
             if (target.borderColor !== "black") {
                 target.boxShadow = "";
@@ -43,10 +56,16 @@
     // Init block addon
     function init() {
         for (const div of blockList) {
+            // Init const
             const target = div.style;
+            const targetP = div.children[0].style;
+            // Style Div
             target.backgroundColor = "transparent";
             target.boxShadow = "";
             target.borderColor = "#888";
+            // Style p
+            targetP.backgroundColor = "#dfdfdf";
+            targetP.color = "black";
         }
     }
 
