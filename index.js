c98674cced10dab96b33f35cfab30f11d2764b4a
Nicolas Marteau <nico_mart@hotmail.fr>
2019-05-17 10:52:10 +0200

    chore: add httpServer.js file

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 // Required Node.js dependencies
 const { join } = require("path");
-const { readFile } = require("fs").promises;
 
 // Required Third-Party dependencies
 const Addon = require("@slimio/addon");
 const alert = require("@slimio/alert");
 const metrics = require("@slimio/metrics");
-const polka = require("polka");
-const send = require("@polka/send-type");
-const sirv = require("sirv");
 const { yellow, red } = require("kleur");
-const { json } = require("body-parser");
-
-// Require Internal dependencies
-const { addonBuilder, alarmBuilder, entityBuilder } = require("./src/utils");
 
 // Constants
 const PORT = process.env.PORT || 8000;
-const VIEWS_DIR = join(__dirname, "views");
+const server = require("./src/httpServer");
 
 // Globlas
-let server;
 let intervalId;
 
 // Create addon
@@ -58,59 +49,9 @@ ihm.on("awake", () => {
 
 // Catch start event!
 ihm.on("start", async() => {
-    // Read index
-    const views = await readFile(join(VIEWS_DIR, "index.html"), { encoding: "utf8" });
-    server = polka();
-
-    // Create POLKA server
-    server
-        .use(sirv(join(__dirname, "public")))
-        .use(json({ type: "application/json" }))
-        .post("/remove", async(req, res) => {
-            await ihm.sendOne(`events.remove_${req.body.type}`, [`2#${req.body.cid}`]);
-
-            send(res, 200);
-        })
-        .get("/", (req, res) => {
-            send(res, 200, views, { "Content-Type": "text/html" });
-        })
-        .get("/addons", async(req, res) => {
-            // eslint-disable-next-line func-names
-            // Add list addon to addonsList
-            /** @type {string[]} */
-            const addonsList = await ihm.sendOne("gate.list_addons");
-            // Loop on all addons
-            const _p = [];
-            for (const addon of addonsList) {
-                if (addon === "ihm") {
-                    continue;
-                }
-                _p.push(ihm.sendOne(`${addon}.get_info`));
-            }
-            const div = await Promise.all(_p);
-            const ret = addonBuilder(div);
-            // Send
-            send(res, 200, ret);
-        })
-        .get("/alarm", async(req, res) => {
-            /** @type {Object[]} */
-            const alarms = alarmBuilder(await ihm.sendOne("events.get_alarms"));
-
-            send(res, 200, alarms);
-        })
-        .get("/entities", async(req, res) => {
-            const entities = entityBuilder(await ihm.sendOne("events.search_entities"));
-
-            send(res, 200, entities);
-        })
-        .get("/test", async(req, res) => {
-            const ret = await ihm.sendOne("events.remove_alarm");
-            console.log(ret);
-        })
-        .listen(PORT, () => {
-            console.log(`Connect to : ${yellow(`http://localhost:${PORT}`)}`);
-        });
-
+    server.listen(PORT, () => {
+        console.log(`Connect to : ${yellow(`http://localhost:${PORT}`)}`);
+    });
     ihm.ready();
 });
 
---
+++ src/httpServer.js
@@ -0,0 +1,73 @@
+// Require Node.js dependencies
+const { join } = require("path");
+const { readFile } = require("fs").promises;
+
+// Require Third-Party dependencies
+const Addon = require("@slimio/addon");
+const polka = require("polka");
+const send = require("@polka/send-type");
+const sirv = require("sirv");
+const { json } = require("body-parser");
+
+// Require Internal dependencies
+const { addonBuilder, alarmBuilder, entityBuilder } = require("./utils");
+
+// Constants
+const VIEWS_DIR = join(__dirname, "..", "views");
+
+// Read index
+async function readHtml() {
+    const views = await readFile(join(VIEWS_DIR, "index.html"), { encoding: "utf8" });
+
+    return views;
+}
+
+// Create POLKA server
+server = polka();
+server
+    .use(sirv(join(__dirname, "..", "public")))
+    .use(json({ type: "application/json" }))
+    .post("/remove", async(req, res) => {
+        await ihm.sendOne(`events.remove_${req.body.type}`, [`2#${req.body.cid}`]);
+
+        send(res, 200);
+    })
+    .get("/", async(req, res) => {
+        send(res, 200, await readHtml(), { "Content-Type": "text/html" });
+    })
+    .get("/addons", async(req, res) => {
+        // eslint-disable-next-line func-names
+        // Add list addon to addonsList
+        /** @type {string[]} */
+        const addonsList = await ihm.sendOne("gate.list_addons");
+        // Loop on all addons
+        const _p = [];
+        for (const addon of addonsList) {
+            if (addon === "ihm") {
+                continue;
+            }
+            _p.push(ihm.sendOne(`${addon}.get_info`));
+        }
+        const div = await Promise.all(_p);
+        const ret = addonBuilder(div);
+        // Send
+        send(res, 200, ret);
+    })
+    .get("/alarm", async(req, res) => {
+        /** @type {Object[]} */
+        const alarms = alarmBuilder(await ihm.sendOne("events.get_alarms"));
+
+        send(res, 200, alarms);
+    })
+    .get("/entities", async(req, res) => {
+        const entities = entityBuilder(await ihm.sendOne("events.search_entities"));
+
+        send(res, 200, entities);
+    })
+    .get("/test", async(req, res) => {
+        const ret = await ihm.sendOne("events.remove_alarm");
+        console.log(ret);
+    });
+
+module.exports = server;
+
