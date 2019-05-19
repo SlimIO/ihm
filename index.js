d17988c49b3856e2c00da922dbceb78953161b1b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-05-19 04:38:56 +0200

    refactor: addon Wrapper

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// Required Third-Party dependencies
+// Required Third-Party Dependencies
 const Addon = require("@slimio/addon");
 
+// Require Internal Dependencies
+const httpServer = require("./src/httpServer")(ihm);
+
 // Create addon
-const ihm = new Addon("ihm");
+const ihm = new Addon("ihm")
+    .lockOn("gate")
+    .lockOn("events");
 
 // CONSTANTS
 const PORT = process.env.PORT || 8000;
-const server = require("./src/httpServer")(ihm);
 
 // Catch start event!
-ihm.on("start", async() => {
-    server.listen(PORT, () => {
+ihm.on("start", () => {
+    httpServer.listen(PORT, () => {
         console.log(`IHM Http Server started at: ${`http://localhost:${PORT}`}`);
     });
     ihm.ready();
 });
 
 // Catch stop event
-ihm.on("stop", async() => {
-    server.server.close();
+ihm.on("stop", () => {
+    httpServer.server.close();
 });
 
 // Export addon for SlimIO Core.
