c2b48e97e8056910eca48af4b901153c6a50b028
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-06-26 23:40:28 +0200

    chore: start working on addons table

--- public/js/master.js
+++ public/js/master.js
@@ -1,4 +1,38 @@
+async function dashboard() {
+    const addons = await fetch("/addons").then((raw) => raw.json());
+    console.log(addons);
+
+    const addonsTable = document.querySelector("#addons_table > tbody");
+    for (const addon of addons) {
+        const row = addonsTable.insertRow();
+        const start = new Date(addon.lastStart).toISOString();
+        const stop = addon.lastStop === null ? "N/A" : new Date(addon.lastStop).toISOString();
+
+        row.insertCell(0);
+        row.insertCell(1).appendChild(document.createTextNode(addon.name));
+        const tdVer = row.insertCell(2);
+        tdVer.classList.add("center");
+        tdVer.appendChild(document.createTextNode(addon.version));
+
+        const tdContainer = row.insertCell(3);
+        tdContainer.classList.add("center");
+        tdContainer.appendChild(document.createTextNode(addon.containerVersion));
+
+        row.insertCell(4).appendChild(document.createTextNode(start));
+        row.insertCell(5).appendChild(document.createTextNode(stop));
+    }
+}
+
+async function alarmconsole() {
+    console.log("alarmconsole js code!");
+}
+
+async function metrics() {
+    console.log("metrics js code!");
+}
+
 let activePage = null;
+const pageExecutor = { dashboard, alarmconsole, metrics };
 
 function menuEventClick() {
     const currPage = this.getAttribute("data-menu");
@@ -17,6 +51,14 @@ async function loadPage(name) {
 
     const HTMLContent = await fetch(`/module/${name}`).then((raw) => raw.text());
     mainElement.innerHTML = HTMLContent;
+    if (Reflect.has(pageExecutor, name)) {
+        try {
+            await pageExecutor[name]();
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
 
     activePage = name;
 }
