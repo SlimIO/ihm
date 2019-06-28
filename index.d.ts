78b470db18a7b695f77c57d5899108fc1c7d2718
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-06-28 22:54:11 +0200

    chore: implement i18n

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare namespace IHM {
+    interface i18n {
+        format: string;
+        keys: {
+            activity_overview: string;
+            search: string;
+            menu: {
+                dashboard: string;
+                alarmconsole: string;
+                alerting: string;
+                metrics: string;
+            }
+        }
+    }
+}
+
+export = IHM;
+export as namespace IHM;
--- src/httpServer.js
+++ src/httpServer.js
@@ -8,6 +8,10 @@ const send = require("@polka/send-type");
 const sirv = require("sirv");
 const zup = require("zup");
 
+// Require Internal Dependencies
+/** @type {IHM.i18n} */
+const i18n = require("../i18n/english.json");
+
 // CONSTANTS
 const PUBLIC_DIR = join(__dirname, "..", "public");
 const VIEWS_DIR = join(__dirname, "..", "views");
@@ -40,16 +44,40 @@ async function getActivityOverview(ihm) {
  */
 function exportServer(ihm) {
     const httpServer = polka();
+
+    // Serve static assets!
     httpServer.use(sirv(PUBLIC_DIR, { dev: true }));
 
     httpServer.get("/", async(req, res) => {
         try {
+            console.time("get_home");
             const [views, data] = await Promise.all([
                 readFile(join(VIEWS_DIR, "index.html"), "utf-8"),
                 getActivityOverview(ihm)
             ]);
 
-            send(res, 200, zup(views)(data), { "Content-Type": "text/html" });
+            const menu = {
+                dashboard: {
+                    title: i18n.keys.menu.dashboard,
+                    icon: "icon-home"
+                },
+                alarmconsole: {
+                    title: i18n.keys.menu.alarmconsole,
+                    icon: "icon-bell-alt"
+                },
+                alerting: {
+                    title: i18n.keys.menu.alerting,
+                    icon: "icon-eye"
+                },
+                metrics: {
+                    title: i18n.keys.menu.metrics,
+                    icon: "icon-chart-line"
+                }
+            };
+
+            const renderedHTML = zup(views)(Object.assign(data, { menu, i18n }));
+            send(res, 200, renderedHTML, { "Content-Type": "text/html" });
+            console.timeEnd("get_home");
         }
         catch (err) {
             send(res, 500, err.message);
