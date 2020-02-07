eca818ee845ceed394306b5175701c59d844935b
Malaj Alexandre <alexandre.malaj@gmail.com>
2020-02-07 10:55:16 +0100

    chore: working on addon option menu

--- public/components/widgets/addons.js
+++ public/components/widgets/addons.js
@@ -10,38 +10,27 @@ class WidgetAddon extends HTMLElement {
         console.log("widget addon start");
         const tmpl = document.getElementById("widget_addons");
         const clone = tmpl.content.cloneNode(true);
+        console.log(clone);
 
         console.log("load addons...");
         const addons = await fetch("/addons").then((raw) => raw.json());
     
         const tbody = clone.querySelector("table > tbody");
+        const fragment = document.createDocumentFragment();
         for (const addon of addons) {
-            const row = tbody.insertRow();
-    
+            
             const start = formatDate(addon.lastStart);
-    
-            const stateTd = row.insertCell(0);
-            const stateBull = createFastElement("div", { classList: ["state"] });
-            stateBull.appendChild(createFastElement("i", { classList: ["icon-ok"] }));
-            stateTd.appendChild(stateBull);
-    
-            row.insertCell(1).appendChild(document.createTextNode(addon.name));
-            const tdVer = row.insertCell(2);
-            tdVer.classList.add("center");
-            tdVer.appendChild(document.createTextNode(addon.version));
-    
-            const tdContainer = row.insertCell(3);
-            tdContainer.classList.add("center");
-            tdContainer.appendChild(document.createTextNode(addon.containerVersion));
-    
-            const startRow = row.insertCell(4);
-            startRow.classList.add("center");
-            startRow.appendChild(document.createTextNode(start));
-    
-            const gearTd = row.insertCell(5);
-            gearTd.appendChild(createFastElement("i", { classList: ["icon-cog"] }));
-        }
 
+            const cloneLine = clone.getElementById("table-line").content.cloneNode(true);
+            cloneLine.querySelector(".state").appendChild(createFastElement("i", { classList: ["icon-ok"] }));
+            cloneLine.querySelector(".name").textContent = addon.name;
+            cloneLine.querySelector(".version").textContent = addon.version;
+            cloneLine.querySelector(".addon-version").textContent = addon.containerVersion;
+            cloneLine.querySelector(".last-start").textContent = start;
+    
+            fragment.appendChild(cloneLine);
+        }
+        tbody.appendChild(fragment);
         this.attachShadow({ mode: "open" }).appendChild(clone);
     }
 }
