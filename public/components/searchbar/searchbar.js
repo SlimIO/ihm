333c0ef58784a8436539559d3d47dfbdc13cdef0
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-06-13 21:48:22 +0200

    chore: start working on the main search bar

--- public/components/searchbar/searchbar.js
+++ public/components/searchbar/searchbar.js
@@ -2,12 +2,40 @@
 class SearchBar extends HTMLElement {
     constructor() {
         super();
-
         const tmpl = document.getElementById("searchbar");
         const clone = tmpl.content.cloneNode(true);
 
-        // Attach shadow root
+        // elements
+        this.input = clone.querySelector("input.search-bar");
+        this.resultsContainer = clone.querySelector(".search-results");
+        this.filterSearchGroup = clone.querySelector(".search-results > #helper");
+
+        // When we click in the Searchbar, then we want to open the results container
+        this.addEventListener("click", () => {
+            if (this.resultsContainer.classList.contains("hidden")) {
+                this.filterSearchGroup.classList.remove("hidden");
+                this.resultsContainer.classList.remove("hidden");
+            }
+        });
+
+        // When we click outside of searchbar then we want to close the results container
+        document.addEventListener("click", (event) => {
+            if (!this.contains(event.target) && !this.resultsContainer.classList.contains("hidden")) {
+                this.close();
+            }
+        });
+
+        this.input.addEventListener("keyup", (event) => {
+
+        });
+
         this.attachShadow({ mode: "open" }).appendChild(clone);
+    }
+
+    close() {
+        this.results_container.classList.add("hidden");
+        this.input.value = "";
+        this.input.blur();
     }
 }
 
